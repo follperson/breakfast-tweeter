ffa9eaf39697f1f509718ca87103873cfdc22382
follperson <follmann.andrew@gmail.com>
2022-04-07 13:56:20 -0400

    adjust cli and add n tweets to put option

--- tweeter.py
+++ tweeter.py
@@ -11,11 +11,18 @@ def cli():
         Markov Tweeter
     """)
     parser.add_argument('file', type=str, default='corpus/corpus.txt')
-    parser.add_argument('state', type=int, default=3)  # order arg. default provided as example
-    parser.add_argument('length', type=int, default=280)  # length arg. default provided as example
-    parser.add_argument('s', type=int, default=60*60)  # time between tweets
+
+    parser.add_argument('--state', type=int, default=3)  # order arg. default provided as example
+    parser.add_argument('--length', type=int, default=280)  # length arg. default provided as example
+    parser.add_argument('-n', type=int, default=-1)  # length arg. default provided as example
+    parser.add_argument('-s', type=int, default=60*60*24/4)  # time between tweets
+
     args = parser.parse_args()
-    tweet_from_file(args.file,message_size=args.length, n_tweets=1, chain_size=args.state, wait_sec=args.s)
+    if args.n <= 0:
+        n_tweets = 1e6
+    else:
+        n_tweets = args.n
+    tweet_from_file(args.file,message_size=args.length, n_tweets=n_tweets, chain_size=args.state, wait_sec=args.s)
 
 
 if __name__ == '__main__':
